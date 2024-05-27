a5576145a8467077f6309ae1ab65a214bb7af0d9
Gautameranpariya <ranpariyagautam23@gmail.com>
2024-05-27 17:58:51 +0530

    add slace on axios file -- gautam

--- React-Redux-Scss-Apis-FireBased-GoogleLogin/src/axios/axiosInstance.jsx
+++ React-Redux-Scss-Apis-FireBased-GoogleLogin/src/axios/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance :)
 const Instance = axios.create({
-    baseURL: 'https://unified-authentication.onrender.com',     // Base URL for all requests :)
+    baseURL: 'https://unified-authentication.onrender.com/',     // Base URL for all requests :)
     // headers: {
     //     'Content-Type': 'application/json',                  // Default headers for all requests :)
     //     'Authorization': 'Bearer <your_token_here>'
