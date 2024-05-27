2e3dde360c512dde6e540fee1c52f1bfd8c01424
Gautameranpariya <ranpariyagautam23@gmail.com>
2024-05-27 12:56:54 +0530

    update comments on firbase file --gautam

--- React-Redux-Scss-Apis-FireBased-GoogleLogin/firebaseConfig.js
+++ React-Redux-Scss-Apis-FireBased-GoogleLogin/firebaseConfig.js
@@ -1,14 +1,10 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// import { getAuth } from 'firebase/auth'
 
-
-// Your web app's Firebase configuration
-
+ // Update Your own Firebase App Configuration :)
 const firebaseConfig = {
-  apiKey:"AIzaSyBeERxwP7H3NoeaXfbuTLCXLMJyOUzYas8",
-  authDomain:"react-redux--auth.firebaseapp.com",
-  projectId:"react-redux--auth",
+  apiKey:"AIzaSyBeERxwP7H3NoeaXfbuTLCXLMJyOUzYas8",     
+  authDomain:"react-redux--auth.firebaseapp.com",       
+  projectId:"react-redux--auth",                        
   storageBucket:"react-redux--auth.appspot.com",
   messagingSenderId:"734348078557",
   appId:"1:734348078557:web:fc7eba2b67612b4c401a34",
@@ -18,13 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-// export const auth = getAuth(app);
 
-
-
-
-// const provider = new GoogleAuthProvider();
-// const analytics = getAnalytics(app);
-// const provider = new GoogleAuthProvider;
 
 
