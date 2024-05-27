0d9d8535c2724c4395015de9bf7cdce9f8fda641
Gautameranpariya <ranpariyagautam23@gmail.com>
2024-05-27 17:57:52 +0530

    update backend url --gautam

--- React-Redux-Scss-Apis-FireBased-GoogleLogin/src/axios/axiosInstance.jsx
+++ React-Redux-Scss-Apis-FireBased-GoogleLogin/src/axios/axiosInstance.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an instance :)
 const Instance = axios.create({
-    baseURL: 'http://localhost:5000/api/',                      // Base URL for all requests :)
+    baseURL: 'https://unified-authentication.onrender.com',     // Base URL for all requests :)
     // headers: {
     //     'Content-Type': 'application/json',                  // Default headers for all requests :)
     //     'Authorization': 'Bearer <your_token_here>'
